173e78b9db3caac7aab922f6eb4fc81a366679c5
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2023-03-05 12:55:15 -0600

    Creating frontend post option

---
+++ frontend/api/axios.config.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const backendAPI = "http://localhost:4000/api";
+
+const client = axios.create({
+	baseURL: `${backendAPI}`,
+	headers: {
+		"Content-type": "application/json",
+	},
+});
+
+export default client;
